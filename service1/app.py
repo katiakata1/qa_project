dcb4f51a002bcdc7ac6c70c9d3dd4d1e40f8713e
Jekaterina <bujanovakatia@gmail.com>
2021-03-01 13:51:15 +0000

    sql instance created

--- service1/app.py
+++ service1/app.py
@@ -4,7 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@database/lists'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.233.220.25/lists'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
@@ -13,7 +13,6 @@ class Lists(db.Model):
     spirit = db.Column(db.String(30), nullable=False)
     volume = db.Column(db.String(30), nullable=False)
     mixer = db.Column(db.String(5000), nullable=False)
-
 
 
 @app.route('/', methods=['GET'])
@@ -29,11 +28,11 @@ def index():
     return render_template('index.html', spirit=spirit.text, volume = volume.text, mixer=mixer.text)
 
 
-
 @app.route('/list', methods=['GET', 'POST'])
 def list():
     spiritData = Lists.query.order_by(Lists.id.desc()).limit(5)
     return render_template('list.html', lists=spiritData)
+
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0')
