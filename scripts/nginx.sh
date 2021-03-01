aeb4b25f5a86dc6e89f8aacaf0e6bb905f3b14ff
Jekaterina <bujanovakatia@gmail.com>
2021-03-01 13:31:28 +0000

    nginx changed

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/jenkins_agent_key swarm-nginx << EOF
+ssh 10.138.0.18 << EOF
 
 sudo rm -rf qa_project
 
-git clone https://github.com/katiakata1/qa_project.git
+git clone https://github.com/katiakata1/qa_project
 
 cd qa_project/service6
 
 sudo docker-compose up -d
+
 EOF
